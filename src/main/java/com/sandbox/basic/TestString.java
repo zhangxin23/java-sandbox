9abc79fbb9a8658f8ecf8de61ad087cf6c21b177
zhangxin <xin_zhang23@126.com>
2016-01-27 22:23:05 +0800

    string

---
+++ src/main/java/com/sandbox/basic/TestString.java
@@ -0,0 +1,21 @@
+package com.sandbox.basic;
+
+/**
+ * Created by zhangxin on 16/1/27.
+ */
+public class TestString {
+    String name;
+
+    public TestString(String personName) {
+        name = personName;
+    }
+
+    public String greet(String yourName) {
+        return String.format("Hi %s, my name is %s", name, yourName);
+    }
+
+    public static void main(String ... args) {
+        System.out.println(new TestString("first").greet("1"));
+    }
+
+}
