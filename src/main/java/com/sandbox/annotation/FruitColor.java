91fbbf104a4ae8ce1b1182cc27cd341f37d16aea
zhangxin5 <zhangxin5@kingsoft.com>
2016-02-15 14:44:39 +0800

    add some code

--- src/main/java/com/sandbox/annotation/FruitColor.java
+++ src/main/java/com/sandbox/annotation/FruitColor.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface FruitColor {
-    public enum Color {BLUE, RED, GREEN};
+    public enum Color {BLUE, RED, GREEN}
 
     Color fruitColor() default Color.GREEN;
 }
--- src/main/java/com/sandbox/annotation/FruitInfoUtil.java
+++ src/main/java/com/sandbox/annotation/FruitInfoUtil.java
@@ -6,24 +6,35 @@ import java.lang.reflect.Field;
  * Created by zhangxin on 15-4-16.
  */
 public class FruitInfoUtil {
-    public static void getFruitInfo(Class<?> clazz){
 
-        String strFruitName=" 水果名称：";
-        String strFruitColor=" 水果颜色：";
-        String strFruitProvicer="供应商信息：";
+    public static void getFruitInfo(Class<?> clazz) {
+
+        String strFruitName = " 水果名称：";
+        String strFruitColor = " 水果颜色：";
+        String strFruitProvicer = "供应商信息：";
 
         Field[] fields = clazz.getDeclaredFields();
 
-        for(Field field :fields){
-            if(field.isAnnotationPresent(FruitName.class)){
-                FruitName fruitName = (FruitName) field.getAnnotation(FruitName.class);
-                strFruitName=strFruitName+fruitName.value();
+        for(Field field: fields) {
+            if(field.isAnnotationPresent(FruitName.class)) {
+                FruitName fruitName = (FruitName)field.getAnnotation(FruitName.class);
+                strFruitName = strFruitName + fruitName.value();
                 System.out.println(strFruitName);
-            }
-            else if(field.isAnnotationPresent(FruitColor.class)){
-                FruitColor fruitColor= (FruitColor) field.getAnnotation(FruitColor.class);
-                strFruitColor=strFruitColor+fruitColor.fruitColor().toString();
+            } else if(field.isAnnotationPresent(FruitColor.class)) {
+                FruitColor fruitColor = (FruitColor)field.getAnnotation(FruitColor.class);
+                strFruitColor = strFruitColor + fruitColor.fruitColor().toString();
                 System.out.println(strFruitColor);
+            }
+        }
+
+        Field[] fields_1 = clazz.getDeclaredFields();
+        for(Field field: fields) {
+            if(field.isAnnotationPresent(FruitName.class)) {
+                FruitName fruitName = field.getAnnotation(FruitName.class);
+                String tmp = fruitName.value();
+            } else if(field.isAnnotationPresent(FruitColor.class)) {
+                FruitColor fruitColor = field.getAnnotation(FruitColor.class);
+                String tmp = fruitColor.fruitColor().toString();
             }
         }
     }
