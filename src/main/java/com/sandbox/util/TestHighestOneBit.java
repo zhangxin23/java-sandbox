7067ffe4c2f22e06334a615abb6d457fd41606c8
zhangxin <xin_zhang23@126.com>
2016-07-12 10:19:18 +0800

    test Integer.highestOneBit(int value)

---
+++ src/main/java/com/sandbox/util/TestHighestOneBit.java
@@ -0,0 +1,16 @@
+package com.sandbox.util;
+
+/**
+ * User: zhangxin
+ * Date: 2016-07-12
+ * Time: 09:43:00
+ */
+public class TestHighestOneBit {
+
+    public static void main(String[] args) {
+        System.out.println("5: " + Integer.highestOneBit(5));
+        System.out.println("6: " + Integer.highestOneBit(6));
+        System.out.println("7: " + Integer.highestOneBit(7));
+        System.out.println("8: " + Integer.highestOneBit(8));
+    }
+}
