f415544c2220ce4b8d45b91ad88e7606edf97293
zhangxin <xin_zhang23@126.com>
2020-07-20 14:24:48 +0800

    java 8 test

---
+++ src/main/java/com/sandbox/java8/First.java
@@ -0,0 +1,15 @@
+package com.sandbox.java8;
+
+/**
+ * @author zhangxin
+ * @version 1.0
+ * @time 2020/7/17 14:43
+ */
+public class First {
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello java 8");
+    }
+}
